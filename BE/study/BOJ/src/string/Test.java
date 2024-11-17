01c6c30bfa533e85f952fd7b1dc34ad73b802f30
DESKTOP-FC1IP11\rhqud <rhqudgus99@gmail.com>
2024-11-17 22:58:53 +0900

    test

---
+++ BE/study/BOJ/src/string/Test.java
@@ -0,0 +1,16 @@
+package string;
+
+public class Test {
+	public static void main(String[] args) {
+		
+		int a = 5;	
+		
+		int b = a--;
+		
+		System.out.println(a);
+		System.out.println(b);
+		
+	
+	}
+
+}
