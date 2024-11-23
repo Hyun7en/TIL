59d0775bbb74c22081f44cb2a45f064ca21a9851
DESKTOP-FC1IP11\rhqud <rhqudgus99@gmail.com>
2024-11-23 19:12:04 +0900

    test01

--- BE/study/BOJ/src/string/Test.java
+++ BE/study/BOJ/src/string/Test.java
@@ -3,12 +3,6 @@ package string;
 public class Test {
 	public static void main(String[] args) {
 		
-		int a = 5;	
-		
-		int b = a--;
-		
-		System.out.println(a);
-		System.out.println(b);
 		
 	
 	}
