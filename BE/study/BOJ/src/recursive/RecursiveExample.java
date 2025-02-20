cff21933d3b736dba8d72d0bb8d11910560c01a1
Hyun7en <rhqudgus99@gmail.com>
2025-02-20 18:50:28 +0900

    feat: Add recursive example

---
+++ BE/study/BOJ/src/recursive/RecursiveExample.java
@@ -0,0 +1,19 @@
+package recursive;
+
+public class RecursiveExample {
+	
+	 // 재귀함수를 이용한 팩토리얼
+    public static long factorial(int n) {
+    	if( n <= 1) {
+    		return n;
+    	}
+    		
+    	
+    	
+    }
+
+    public static void main(String[] args) {
+        int num = 5;
+        System.out.println(num + "! = " + factorial(num));
+    }
+}
