215a954211772876fd3ab892a66a520fb8464530
Hyun7en <rhqudgus99@gmail.com>
2025-05-09 16:48:10 +0900

    test: outer, inner class test

---
+++ src/test/Outer.java
@@ -0,0 +1,13 @@
+package test;
+
+public class Outer {
+    public static void main(String[] args) {
+        System.out.println("outer");
+    }
+
+    public static class inner {
+        public static void main(String[] args) {
+            System.out.println("inner");
+        }
+    }
+}
