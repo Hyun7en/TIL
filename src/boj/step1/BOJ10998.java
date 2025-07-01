b245f14efa10a54729ec42826413fff5a1003830
Hyun7en <rhqudgus99@gmail.com>
2025-07-01 22:47:22 +0900

    20250701: step1 BOJ 10998

--- src/boj/step1/BOJ10998.java
+++ src/boj/step1/BOJ10998.java
@@ -11,8 +11,36 @@ package boj.step1;
 첫째 줄에 A×B를 출력한다.
  */
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
 public class BOJ10998 {
-    public static void main(String[] args){
+//    public static void main(String[] args){
+//        Scanner sc = new Scanner(System.in);
+//
+//        int A = sc.nextInt();
+//        int B = sc.nextInt();
+//
+//        System.out.println(A * B);
+//
+//    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+
+        String N = br.readLine();
+
+        StringTokenizer st = new StringTokenizer(N);
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        br.close();
+
+        System.out.println(A * B);
 
     }
 }
