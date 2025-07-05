fd13d55dc9c36602c58a0f729114e9a1c5b7c413
Hyun7en <rhqudgus99@gmail.com>
2025-07-05 23:25:17 +0900

    20250705: step1 BOJ 10869

--- src/boj/step1/BOJ10869.java
+++ src/boj/step1/BOJ10869.java
@@ -1,4 +1,36 @@
 package boj.step1;
 
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/*
+문제
+두 자연수 A와 B가 주어진다. 이때, A+B, A-B, A*B, A/B(몫), A%B(나머지)를 출력하는 프로그램을 작성하시오.
+
+입력
+두 자연수 A와 B가 주어진다. (1 ≤ A, B ≤ 10,000)
+
+출력
+첫째 줄에 A+B, 둘째 줄에 A-B, 셋째 줄에 A*B, 넷째 줄에 A/B, 다섯째 줄에 A%B를 출력한다.
+ */
 public class BOJ10869 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String N = br.readLine();
+
+        StringTokenizer st = new StringTokenizer(N);
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        System.out.println(A + B);
+        System.out.println(A - B);
+        System.out.println(A * B);
+        System.out.println(A / B);
+        System.out.println(A % B);
+
+    }
 }
