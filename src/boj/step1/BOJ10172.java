4e5994829fdb5fb8f3b2fbdc7f7b8aa70bdcd842
Hyun7en <rhqudgus99@gmail.com>
2025-07-07 23:55:31 +0900

    20250707: step1 BOJ 10172

---
+++ src/boj/step1/BOJ10172.java
@@ -0,0 +1,24 @@
+package boj.step1;
+
+/*
+시간 제한	메모리 제한	제출	정답	맞힌 사람	정답 비율
+1 초	256 MB	519991	220061	193169	43.020%
+문제
+아래 예제와 같이 개를 출력하시오.
+
+입력
+없음.
+
+출력
+개를 출력한다.
+ */
+public class BOJ10172 {
+    public static void main(String[] args){
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+
+    }
+}
