2cb0c45efbda849dba386fc5956aa5b3fc5c54b7
Hyun7en <rhqudgus99@gmail.com>
2025-07-07 23:47:12 +0900

    20250707: step1 BOJ 10171

---
+++ src/boj/step1/BOJ10171.java
@@ -0,0 +1,21 @@
+package boj.step1;
+
+/*
+문제
+아래 예제와 같이 고양이를 출력하시오.
+
+입력
+없음.
+
+출력
+고양이를 출력한다.
+ */
+public class BOJ10171 {
+    public static void main(String[] args){
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+
+    }
+}
