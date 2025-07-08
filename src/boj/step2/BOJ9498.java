8aca16e59bd2d45614837524ba079ef21eec553f
Hyun7en <rhqudgus99@gmail.com>
2025-07-08 18:19:22 +0900

    20250708: step2 BOJ 9498

--- src/boj/step2/BOJ9498.java
+++ src/boj/step2/BOJ9498.java
@@ -1,5 +1,9 @@
 package boj.step2;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /*
 문제(시험 성적)
 시험 점수를 입력받아 90 ~ 100점은 A, 80 ~ 89점은 B, 70 ~ 79점은 C, 60 ~ 69점은 D, 나머지 점수는 F를 출력하는 프로그램을 작성하시오.
@@ -11,7 +15,22 @@ package boj.step2;
 시험 성적을 출력한다.
  */
 public class BOJ9498 {
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+         int score = Integer.parseInt(br.readLine());
+        if( 0 <= score && score <= 100 ) {
+            if (score >= 90) {
+                System.out.println("A");
+            } else if (score >= 80) {
+                System.out.println("B");
+            } else if (score >= 70) {
+                System.out.println("C");
+            } else if (score >= 60) {
+                System.out.println("D");
+            } else {
+                System.out.println("F");
+            }
+        }
 
     }
 }
