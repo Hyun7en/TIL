f810e98758c27a29267cd1f8bcb2fb21073c59d4
Hyun7en <rhqudgus99@gmail.com>
2025-07-10 23:49:11 +0900

    20250710: step3 BOJ 2438

---
+++ src/boj/step03/BOJ2438.java
@@ -0,0 +1,31 @@
+package boj.step03;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/*
+**문제(별 찍기 - 1)**
+첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제
+
+**입력**
+첫째 줄에 N(1 ≤ N ≤ 100)이 주어진다.
+
+**출력**
+첫째 줄부터 N번째 줄까지 차례대로 별을 출력한다.
+ */
+public class BOJ2438 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        br.close();
+        String star = "";
+
+        for(int i = 0; i < N; i++){
+            star += "*";
+            System.out.println(star);
+        }
+
+    }
+}
