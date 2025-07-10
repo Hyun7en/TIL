f3b415a24e2d0a3739d9639242823a31d78179fa
Hyun7en <rhqudgus99@gmail.com>
2025-07-11 00:38:31 +0900

    20250710: step3 BOJ 10952

---
+++ src/boj/step03/BOJ10952.java
@@ -0,0 +1,43 @@
+package boj.step03;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/*
+문제
+두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+
+입력
+입력은 여러 개의 테스트 케이스로 이루어져 있다.
+
+각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
+
+입력의 마지막에는 0 두 개가 들어온다.
+
+출력
+각 테스트 케이스마다 A+B를 출력한다.
+ */
+public class BOJ10952 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while(true) {
+            String N = br.readLine();
+
+            StringTokenizer st = new StringTokenizer(N);
+
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+
+            if(A + B == 0){
+                break;
+            }
+
+            System.out.println(A + B);
+        };
+        br.close();
+
+    }
+}
