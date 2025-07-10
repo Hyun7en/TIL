5330483eb6f8214ca1a98e16fe948699029f5552
Hyun7en <rhqudgus99@gmail.com>
2025-07-10 21:08:11 +0900

    20250710: step3 BOJ 10950

--- src/boj/step3/BOJ10950.java
+++ src/boj/step3/BOJ10950.java
@@ -1,5 +1,10 @@
 package boj.step3;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 /*
 **문제(A+B - 3)**
 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
@@ -13,4 +18,23 @@ package boj.step3;
 각 테스트 케이스마다 A+B를 출력한다.
  */
 public class BOJ10950 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < T; i++){
+            String N = br.readLine();
+
+            StringTokenizer st = new StringTokenizer(N);
+
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+
+            System.out.println(A + B);
+
+        }
+
+        br.close();
+    }
 }
