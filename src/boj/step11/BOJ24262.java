f6e44cc1331ff067f7c5e9c4454a642794b7d345
Hyun7en <rhqudgus99@gmail.com>
2025-07-17 18:38:22 +0900

    20250717: step11 BOJ 24262

--- src/boj/step11/BOJ24262.java
+++ src/boj/step11/BOJ24262.java
@@ -23,7 +23,7 @@ MenOfPassion(A[], n) {
 
 둘째 줄에 코드1의 수행 횟수를 다항식으로 나타내었을 때, 최고차항의 차수를 출력한다. 단, 다항식으로 나타낼 수 없거나 최고차항의 차수가 3보다 크면 4를 출력한다.
  */
-public class BOJ244262 {
+public class BOJ24262 {
     public static void main(String[] args) {
         System.out.println(1); // 코드1 수행 횟수
         System.out.println(0); // 수행 시간의 최고차항의 차수
