e30032fbf77b06f8073bafc207a135c8b7b6372e
Hyun7en <rhqudgus99@gmail.com>
2025-07-19 00:20:07 +0900

    test

--- src/test/test.java
+++ src/test/test.java
@@ -4,19 +4,6 @@ package test;
 public class test {
     public static void main(String[] args) {
 
-        int c = 0;
-        int i;
-
-        for ( i = 1; i <= 2023; i++) {
-
-            if (i % 4 == 0) {
-                c++;
-            }
-        }
-
-
-        System.out.println(c);
-        System.out.println(i);
     }
 
 
