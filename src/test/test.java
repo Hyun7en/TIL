b0c2c1a4049bc1e9dcbb501707133e03c7a72c58
Byeonghyun Go <rhqudgus99@gmail.com>
2025-09-20 21:06:19 +0900

    test

--- src/test/test.java
+++ src/test/test.java
@@ -3,7 +3,7 @@ package test;
 
 public class test {
     public static void main(String[] args){
-        int n = 5; // 테스트용 숫자
+        int n = 5;
 
         // 1부터 n까지 출력
         for (int i = 1; i <= n; i++) {
