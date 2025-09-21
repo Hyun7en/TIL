69283b81b2261b2e53f4ab79bf4182c56fd0813c
Byeonghyun Go <rhqudgus99@gmail.com>
2025-09-21 21:15:11 +0900

    test

--- src/test/test.java
+++ src/test/test.java
@@ -5,7 +5,7 @@ public class test {
     public static void main(String[] args){
         int n = 5;
 
-        // 1부터 n까지 출력
+        // 1부터 n
         for (int i = 1; i <= n; i++) {
             System.out.println("현재 숫자: " + i);
     }
