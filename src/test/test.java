efeaa212e0862f45ff6dadcec2bf0828c4e3231b
Hyun7en <rhqudgus99@gmail.com>
2025-11-13 22:33:01 +0900

    test

--- src/test/test.java
+++ src/test/test.java
@@ -2,14 +2,15 @@ package test;
 
 
 public class test {
-    public static void main(String[] args){
-        
+    public static void main(String[] args) {
+
         int n = 5;
 
         for (int i = 1; i <= n; i++) {
             System.out.println("현재 숫자: " + i);
+        }
+
+
     }
-
-
 }
 
