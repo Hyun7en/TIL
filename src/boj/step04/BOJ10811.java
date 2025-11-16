98a8dc95c152f1c0d5ae6c6b50791b18d243061d
Hyun7en <rhqudgus99@gmail.com>
2025-11-16 22:29:52 +0900

    20251116: Rename file name

--- src/boj/step04/BOJ10811.java
+++ src/boj/step04/BOJ10811.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class boj10811 {
+public class BOJ10811 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer strtk = new StringTokenizer(br.readLine()," ");
