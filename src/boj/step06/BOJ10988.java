999a823760ca63e06e913131c2659047a9cef93c
Hyun7en <rhqudgus99@gmail.com>
2025-11-24 23:28:03 +0900

    20251124: Step06 BOJ 10988

---
+++ src/boj/step06/BOJ10988.java
@@ -0,0 +1,16 @@
+package boj.step06;
+
+import java.io.*;
+
+public class BOJ10988 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder(br.readLine());
+
+        bw.write(((sb.toString().equals(sb.reverse().toString())) ? 1 : 0) + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
