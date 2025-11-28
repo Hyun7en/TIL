4f3060d32b4d441f3fd6aab516567424711f8563
Hyun7en <rhqudgus99@gmail.com>
2025-11-28 13:24:54 +0900

    test

--- src/test/test.java
+++ src/test/test.java
@@ -3,56 +3,12 @@ package test;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.StringTokenizer;
 
-/*
-도현이는 바구니를 총 N개 가지고 있고, 각각의 바구니에는 1번부터 N번까지 번호가 순서대로 적혀져 있다.
-바구니는 일렬로 놓여져 있고, 가장 왼쪽 바구니를 1번째 바구니, 그 다음 바구니를 2번째 바구니,
-..., 가장 오른쪽 바구니를 N번째 바구니라고 부른다.
 
-도현이는 앞으로 M번 바구니의 순서를 역순으로 만들려고 한다.
-도현이는 한 번 순서를 역순으로 바꿀 때, 순서를 역순으로 만들 범위를 정하고,
-그 범위에 들어있는 바구니의 순서를 역순으로 만든다.
-
-바구니의 순서를 어떻게 바꿀지 주어졌을 때, M번 바구니의 순서를 역순으로 만든 다음,
-바구니에 적혀있는 번호를 가장 왼쪽 바구니부터 출력하는 프로그램을 작성하시오.
- */
 public class test {
     public static void main(String[] args) throws IOException {
-       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-
-        List<Integer> list = new ArrayList<>();
-
-
-        for(int i = 0; i < N; i++){
-            list.add(i + 1);
-        }
-
-        for(int i = 0; i < M; i++){
-            st = new StringTokenizer(br.readLine());
-            int k = Integer.parseInt(st.nextToken());
-            int l = Integer.parseInt(st.nextToken());
-
-            Collections.reverse(list.subList(k - 1, l));
-
-        }
-
-        StringBuilder sb = new StringBuilder();
-
-       for(int x : list){
-           sb.append(x).append(" ");
-       }
-
-        System.out.println(sb);
-        br.close();
 
     }
 }
