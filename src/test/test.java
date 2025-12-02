81f0c73023b7576fc36c40c76718fd13fce328be
Hyun7en <rhqudgus99@gmail.com>
2025-12-02 11:16:24 +0900

    test

--- src/test/test.java
+++ src/test/test.java
@@ -9,7 +9,22 @@ public class test {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        boolean[] arr = new boolean[42];
+        int cnt = 0;
 
+        for(int i = 0 ; i < 10; i++){
+            int n = Integer.parseInt(br.readLine());
+
+            arr[n % 42] = true;
+
+        }
+
+       for(boolean value : arr){
+           if(value) cnt++;
+       }
+
+        System.out.println(cnt);
+        br.close();
     }
 }
 
