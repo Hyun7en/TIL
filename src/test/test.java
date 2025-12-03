c378047d4808a1ef6af25411844a0d7d957b13af
Hyun7en <rhqudgus99@gmail.com>
2025-12-03 23:27:39 +0900

    test

--- src/test/test.java
+++ src/test/test.java
@@ -3,27 +3,29 @@ package test;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 
 public class test {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        boolean[] arr = new boolean[42];
-        int cnt = 0;
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        for(int i = 0 ; i < 10; i++){
-            int n = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
-            arr[n % 42] = true;
+        int[] arr = new int[N];
+
+        for(int i = 0 ; i < N; i++){
+            arr[i] = i + 1;
+        }
+
+        for(int i = 0 ; i < M ; i++){
 
         }
 
-       for(boolean value : arr){
-           if(value) cnt++;
-       }
 
-        System.out.println(cnt);
         br.close();
     }
 }
