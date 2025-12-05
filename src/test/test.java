87a91cc788942b527d1c2b0d916164ee3402803c
Hyun7en <rhqudgus99@gmail.com>
2025-12-06 03:38:29 +0900

    test

--- src/test/test.java
+++ src/test/test.java
@@ -3,39 +3,21 @@ package test;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 
 public class test {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int input = 5; // 입력 5줄
+        String str = br.readLine();
 
-        Character[][] arr = new Character[15][15];
+        StringBuilder sb = new StringBuilder();
 
-        String result = "";
-
-        for(int i = 0 ; i < input ; i++ ){
-            String str = br.readLine();
-
-            for(int j = 0 ; j < str.length() ; j++){
-                arr[i][j] = str.charAt(j);
-            }
+        for(char c = 'a' ; c <= 'z' ; c++){
+            sb.append(str.indexOf(c)).append(" ");
         }
 
-        for(int j = 0; j < 15; j++){
-            for(int i = 0; i < 15; i++){
-                if(arr[i][j] != null) {
-                    result += arr[i][j];
-                }
-            }
-        }
-
-        System.out.println(result);
-
-
-
+        System.out.println(sb);
         br.close();
     }
 }
