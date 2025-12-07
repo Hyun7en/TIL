f5a39b5819c3edc292d5e4f8319bbd412d83e70e
Hyun7en <rhqudgus99@gmail.com>
2025-12-07 23:36:22 +0900

    test

--- src/test/test.java
+++ src/test/test.java
@@ -3,13 +3,30 @@ package test;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 
 public class test {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        
+        int T = Integer.parseInt(br.readLine());
+        StringBuilder result = new StringBuilder();
+        for(int i =  0 ; i < T ; i++ ){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int R = Integer.parseInt(st.nextToken());
+            String S = st.nextToken();
+
+            for(int j = 0 ; j < S.length(); j++ ){
+                char ch = S.charAt(j);
+                for(int k = 0 ; k < R; k++){
+                    result.append(ch);
+                }
+            }
+
+            System.out.println(result);
+        }
+
         br.close();
     }
 }
