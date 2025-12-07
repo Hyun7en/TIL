3e7da0a89512b0d6603ce9a1a9f1bf3baa8a9922
Hyun7en <rhqudgus99@gmail.com>
2025-12-07 17:08:23 +0900

    test

--- src/test/test.java
+++ src/test/test.java
@@ -9,15 +9,7 @@ public class test {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String str = br.readLine();
-
-        StringBuilder sb = new StringBuilder();
-
-        for(char c = 'a' ; c <= 'z' ; c++){
-            sb.append(str.indexOf(c)).append(" ");
-        }
-
-        System.out.println(sb);
+        
         br.close();
     }
 }
