93e742e32c4a852511951c674b51c548c8e2b6de
Hyun7en <rhqudgus99@gmail.com>
2025-12-08 23:38:03 +0900

    test

--- src/test/test.java
+++ src/test/test.java
@@ -10,18 +10,40 @@ public class test {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-       String str = br.readLine();
+        String str = br.readLine();
+        int result = 0;
 
-       StringTokenizer st = new StringTokenizer(str);
+        for(int i = 0 ;  i < str.length(); i++){
 
-        String a = new StringBuilder(st.nextToken()).reverse().toString();
-        String b = new StringBuilder(st.nextToken()).reverse().toString();
-
-        int result = Math.max(Integer.parseInt(a),Integer.parseInt(b));
+            result += num(str.charAt(i));
+        }
 
         System.out.println(result);
 
         br.close();
+    }
+
+    static int num(char c) {
+        switch (c) {
+            case 'A': case 'B': case 'C':
+                return 3;
+            case 'D': case 'E': case 'F':
+                return 4;
+            case 'G': case 'H': case 'I':
+                return 5;
+            case 'J': case 'K': case 'L':
+                return 6;
+            case 'M': case 'N': case 'O':
+                return 7;
+            case 'P': case 'Q': case 'R': case 'S':
+                return 8;
+            case 'T': case 'U': case 'V':
+                return 9;
+            case 'W': case 'X': case 'Y': case 'Z':
+                return 10;
+            default:
+                return 0;
+        }
     }
 }
 
