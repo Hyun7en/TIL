36b4deea739f2058603c7945f1dbf43f90589098
Hyun7en <rhqudgus99@gmail.com>
2025-12-09 21:03:37 +0900

    test

--- src/test/test.java
+++ src/test/test.java
@@ -3,47 +3,21 @@ package test;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 
 public class test {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line;
 
-        String str = br.readLine();
-        int result = 0;
-
-        for(int i = 0 ;  i < str.length(); i++){
-
-            result += num(str.charAt(i));
+        while((line = br.readLine()) != null){
+            System.out.println(line);
         }
 
-        System.out.println(result);
 
         br.close();
     }
 
-    static int num(char c) {
-        switch (c) {
-            case 'A': case 'B': case 'C':
-                return 3;
-            case 'D': case 'E': case 'F':
-                return 4;
-            case 'G': case 'H': case 'I':
-                return 5;
-            case 'J': case 'K': case 'L':
-                return 6;
-            case 'M': case 'N': case 'O':
-                return 7;
-            case 'P': case 'Q': case 'R': case 'S':
-                return 8;
-            case 'T': case 'U': case 'V':
-                return 9;
-            case 'W': case 'X': case 'Y': case 'Z':
-                return 10;
-            default:
-                return 0;
-        }
-    }
+
 }
 
