5cdb1808b3a18e5eef702dd5aa687c432f84cacf
Hyun7en <rhqudgus99@gmail.com>
2025-12-09 23:03:17 +0900

    test

--- src/test/test.java
+++ src/test/test.java
@@ -8,16 +8,31 @@ import java.io.InputStreamReader;
 public class test {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String line;
 
-        while((line = br.readLine()) != null){
-            System.out.println(line);
+        String str = br.readLine().toUpperCase();
+
+        int[] arr = new int[26];
+
+        for(int i = 0 ; i < str.length(); i++ ){
+            arr[str.charAt(i) - 'A']++;
         }
 
+        int max = 0;
+        char result = '?';
+
+        for(int i = 0 ; i < arr.length; i++){
+            if(arr[i] > max){
+                max = arr[i];
+                result = (char)(i + 'A');
+            }else if(arr[i] == max){
+                result = '?';
+            }
+        }
+
+        System.out.println(result);
 
         br.close();
     }
-
 
 }
 
