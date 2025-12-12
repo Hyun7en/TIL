8f51aee0db489f494c08f3d687df431c3f6f2d98
Hyun7en <rhqudgus99@gmail.com>
2025-12-13 00:07:35 +0900

    20251212: Step08 BOJ 2869

--- src/boj/step08/BOJ2869.java
+++ src/boj/step08/BOJ2869.java
@@ -1,10 +1,40 @@
 package boj.step08;
 
-/*
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+/*
+<문제: 달팽이는 올라가고 싶다>
+땅 위에 달팽이가 있다. 이 달팽이는 높이가 V미터인 나무 막대를 올라갈 것이다.
+
+달팽이는 낮에 A미터 올라갈 수 있다. 하지만, 밤에 잠을 자는 동안 B미터 미끄러진다. 또, 정상에 올라간 후에는 미끄러지지 않는다.
+
+달팽이가 나무 막대를 모두 올라가려면, 며칠이 걸리는지 구하는 프로그램을 작성하시오.
+
+<입력>
+첫째 줄에 세 정수 A, B, V가 공백으로 구분되어서 주어진다. (1 ≤ B < A ≤ V ≤ 1,000,000,000)
+
+<출력>
+첫째 줄에 달팽이가 나무 막대를 모두 올라가는데 며칠이 걸리는지 출력한다.
  */
 public class BOJ2869 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        StringTokenizer st = new StringTokenizer(br.readLine()); // A, B, V
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int V = Integer.parseInt(st.nextToken());
+
+        //하루 A - B 만큼, 마지막 날 전에는 V - A
+        //답은
+        int day = (int) Math.ceil((double)(V - A) / (A - B)) + 1;
+
+        System.out.println(day);
+
+        br.close();
     }
 }
