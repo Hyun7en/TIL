d4ba3aa7acb8b911a84a331fa35d3de38fe93a81
Hyun7en <rhqudgus99@gmail.com>
2025-12-15 00:34:13 +0900

    20251215: Step09 BOJ 11653

--- src/boj/step09/BOJ11653.java
+++ src/boj/step09/BOJ11653.java
@@ -1,6 +1,7 @@
 package boj.step09;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 /*
@@ -14,8 +15,24 @@ import java.io.InputStreamReader;
 N의 소인수분해 결과를 한 줄에 하나씩 오름차순으로 출력한다. N이 1인 경우 아무것도 출력하지 않는다.
  */
 public class BOJ11653 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N =  Integer.parseInt(br.readLine());
+
+        for(int i = 2 ; i * i <= N; i++){
+            while(N % i == 0 ){
+                System.out.println(i);
+                N /= i;
+            }
+        }
+
+        //N이 소수로 남은 경우 출력을 위해
+        if(N > 1){
+            System.out.println(N);
+        }
+        br.close();
+
 
 
     }
