fcb97f6206883e6de9b7db68fbc8348ab820a0b6
Hyun7en <rhqudgus99@gmail.com>
2025-12-15 23:53:21 +0900

    20251215: Step10 BOJ 5073

---
+++ src/boj/step10/BOJ5073.java
@@ -0,0 +1,17 @@
+package boj.step10;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/*
+
+ */
+public class BOJ5073 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+
+        br.close();
+    }
+}
