432a33d65ea6e8e5f7283f34d81cd8716ce6faa2
Hyun7en <rhqudgus99@gmail.com>
2025-12-16 21:45:03 +0900

    20251216: Step12 BOJ 19532

---
+++ src/boj/step12/BOJ19532.java
@@ -0,0 +1,17 @@
+package boj.step12;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/*
+
+ */
+public class BOJ19532 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+        br.close();
+    }
+}
