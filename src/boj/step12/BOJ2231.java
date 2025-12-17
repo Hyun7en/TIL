5e035a561830e26443999929e2e4637a7299a298
Hyun7en <rhqudgus99@gmail.com>
2025-12-17 21:58:45 +0900

    20251217: Step12 BOJ 2231

--- src/boj/step12/BOJ2231.java
+++ src/boj/step12/BOJ2231.java
@@ -3,6 +3,7 @@ package boj.step12;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigDecimal;
 
 /*
 <문제: 분해합 >
@@ -23,7 +24,8 @@ public class BOJ2231 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        
+
+
         br.close();
     }
 }
