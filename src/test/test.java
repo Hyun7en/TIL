b18a8057fe45df6996d709237a55fdec1b69c1f0
Hyun7en <rhqudgus99@gmail.com>
2026-01-01 18:01:47 +0900

    test

--- src/test/test.java
+++ src/test/test.java
@@ -6,6 +6,10 @@ import java.io.IOException;
 public class test {
     public static void main(String[] args) throws IOException {
 
+        char a = 'A';
+        int b = a;
+        System.out.println(a);
+        System.out.println(b);
 
     }
 
