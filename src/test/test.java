d5b384f9154585eb0e58fdfff64c247639f77294
Hyun7en <rhqudgus99@gmail.com>
2026-01-05 00:01:50 +0900

    test

--- src/test/test.java
+++ src/test/test.java
@@ -6,28 +6,7 @@ import java.io.IOException;
 public class test {
     public static void main(String[] args) throws IOException {
 
-        allLoop :
-
-        for (int i = 2; i < 10; i++) {
-
-            for (int j = 2; j < 10; j++) {
-
-                if (i == 5) {
-
-
-                    break allLoop;
-
-                }
-
-                System.out.println(i + " * " + j + " = " + (i * j));
-
-            }
-
-        }
-
-
     }
-
 
 }
 
