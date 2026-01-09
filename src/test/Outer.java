ee8a071ab5ec2c0330baf3c494f11e27592a8251
Hyun7en <rhqudgus99@gmail.com>
2026-01-09 21:31:00 +0900

    test

--- src/test/Outer.java
+++ src/test/Outer.java
@@ -2,11 +2,42 @@ package test;
 
 public class Outer {
     private  int n = 10;
+    static int value = 11;
 
-    public  class Inner {
+    //instance inner class
+    public class Inner{
 
         void print(){
             System.out.println(n);
         }
     }
+
+    //static inner class
+    public static class Inner2{
+        void print(){
+            System.out.println(value);
+        }
+    }
+
+    //local inner class
+    public void mathod(){
+        class Inner3{
+            void print(){
+                System.out.println("local");
+            }
+        }
+        new Inner3().print();
+    }
+
+    //anonymous inner class
+    Runnable run = new Runnable() {
+        @Override
+        public void run() {
+            System.out.println("run");
+        }
+    };
+
+    
+
+
 }
