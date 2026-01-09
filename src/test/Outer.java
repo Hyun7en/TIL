0a276aeb1204a518b4737cc6d01965a118fc7279
Hyun7en <rhqudgus99@gmail.com>
2026-01-09 21:15:08 +0900

    test

--- src/test/Outer.java
+++ src/test/Outer.java
@@ -1,13 +1,14 @@
 package test;
 
 public class Outer {
+    private static int n = 10;
     public static void main(String[] args) {
         System.out.println("outer");
     }
 
     public static class Inner {
         public static void main(String[] args) {
-            System.out.println("inner");
+            System.out.println(Outer.n);
         }
     }
 }
