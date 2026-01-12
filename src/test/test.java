564704db85f39f7bdb1c86db1f6c336db8fa94eb
Hyun7en <rhqudgus99@gmail.com>
2026-01-12 21:38:49 +0900

    test

--- src/test/test.java
+++ src/test/test.java
@@ -6,17 +6,18 @@ public class test {
         Integer num = new Integer(4);
         Integer num1 = new Integer(4);
 
-        Integer num2 = Integer.valueOf(4);
-        Integer num3 = Integer.valueOf(4);
+        Integer num2 = Integer.valueOf(222);
+        Integer num3 = Integer.valueOf(222);
 
-        Integer num4 = 4;
-        Integer num5 = 4;
+        Integer num4 = 222;
+        Integer num5 = 222;
 
 
         System.out.println(num == num1);
         System.out.println(num2 == num3);
         System.out.println(num4 == num5);
         System.out.println(num == num2);
+        System.out.println(num2 == num4);
     }
 
 }
