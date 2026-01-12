ba059b6a0cfb7e51e3545cc7c57db9350db70664
Hyun7en <rhqudgus99@gmail.com>
2026-01-12 21:56:16 +0900

    test

--- src/test/test.java
+++ src/test/test.java
@@ -2,22 +2,15 @@ package test;
 
 
 public class test {
+    enum Rainbow{
+        RED,ORANGE
+    }
     public static void main(String[] args)   {
-        Integer num = new Integer(4);
-        Integer num1 = new Integer(4);
+        Rainbow[] arr = Rainbow.values();
 
-        Integer num2 = Integer.valueOf(222);
-        Integer num3 = Integer.valueOf(222);
-
-        Integer num4 = 222;
-        Integer num5 = 222;
-
-
-        System.out.println(num == num1);
-        System.out.println(num2 == num3);
-        System.out.println(num4 == num5);
-        System.out.println(num == num2);
-        System.out.println(num2 == num4);
+        for (Rainbow rb : arr){
+            System.out.println(rb);
+        }
     }
 
 }
