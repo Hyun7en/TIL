d17edb19f05fa76dcdb6e4ea892ed74bff5624c3
Hyun7en <rhqudgus99@gmail.com>
2026-01-12 22:55:15 +0900

    test

--- src/test/test.java
+++ src/test/test.java
@@ -6,10 +6,15 @@ import java.util.Calendar;
 public class test {
 
     public static void main(String[] args)   {
-        Calendar time = Calendar.getInstance();
+        Calendar time1 = Calendar.getInstance();
+        Calendar time2 = Calendar.getInstance();
+        Calendar time3 = Calendar.getInstance();
 
-        System.out.println(time.getTime());
-        
+        time2.set(1982, 2, 19);
+        time3.set(2020, 2, 19);
+
+        System.out.println(time1.before(time2));
+        System.out.println(time1.before(time3));
     }
 
 }
