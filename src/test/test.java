8ec2d52ea97727bd5910e65e6a8f4f05d66ed1e2
Hyun7en <rhqudgus99@gmail.com>
2026-01-12 21:32:25 +0900

    test

--- src/test/test.java
+++ src/test/test.java
@@ -3,7 +3,20 @@ package test;
 
 public class test {
     public static void main(String[] args)   {
+        Integer num = new Integer(4);
+        Integer num1 = new Integer(4);
 
+        Integer num2 = Integer.valueOf(4);
+        Integer num3 = Integer.valueOf(4);
+
+        Integer num4 = 4;
+        Integer num5 = 4;
+
+
+        System.out.println(num == num1);
+        System.out.println(num2 == num3);
+        System.out.println(num4 == num5);
+        System.out.println(num == num2);
     }
 
 }
