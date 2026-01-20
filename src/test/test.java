d1465bf4c6f88d987f93996f96d2bae40582f08f
Hyun7en <rhqudgus99@gmail.com>
2026-01-20 18:28:37 +0900

    test

--- src/test/test.java
+++ src/test/test.java
@@ -1,12 +1,25 @@
 package test;
 
-
-import java.util.Calendar;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class test {
 
-    public static void main(String[] args)   {
-       
+    public static void main(String[] args) throws IOException {
+       Path dir = Paths.get("C:\\project");
+
+        Files.createDirectories(dir);
+
+        Path file = dir.resolve("data.txt");
+
+        if(Files.notExists(file)){
+            Files.createFile(file);
+        }
+
+        System.out.println(file.toString());
+        System.out.println(Files.size(file));
     }
 
 }
