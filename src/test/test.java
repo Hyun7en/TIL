b1e961fb412bd2b70632356b94422e82547feaa5
Hyun7en <rhqudgus99@gmail.com>
2026-01-21 19:25:59 +0900

    test

--- src/test/test.java
+++ src/test/test.java
@@ -1,25 +1,11 @@
 package test;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 public class test {
 
     public static void main(String[] args) throws IOException {
-       Path dir = Paths.get("C:\\project");
 
-        Files.createDirectories(dir);
-
-        Path file = dir.resolve("data.txt");
-
-        if(Files.notExists(file)){
-            Files.createFile(file);
-        }
-
-        System.out.println(file.toString());
-        System.out.println(Files.size(file));
     }
 
 }
