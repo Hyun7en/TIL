e15190d94f76efa953f7adef39155d689ae17efa
Hyun7en <rhqudgus99@gmail.com>
2026-01-23 23:00:34 +0900

    test

--- src/test/test.java
+++ src/test/test.java
@@ -1,24 +1,16 @@
 package test;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.function.DoubleUnaryOperator;
+import java.util.Arrays;
 import java.util.stream.Stream;
 
 public class test{
 
     public static void main(String[] args) throws IOException {
-        ArrayList<Integer> list = new ArrayList<>();
+        String[] arr = {"I    study hard", "You study JAVA", "I am hungry"};
 
-        list.add(1);
-        list.add(2);
-        list.add(3);
-        list.add(4);
-        list.add(5);
-
-        Stream<Integer> stream = list.stream();
-
-        stream.forEach(System.out::println);
+        Stream<String> stream = Arrays.stream(arr);
+        stream.flatMap(s -> Stream.of(s.split("\\s+"))).forEach(System.out::println);
 
     }
 
