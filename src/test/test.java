6a961bade90a334e612708de07c11dda24e5f958
Hyun7en <rhqudgus99@gmail.com>
2026-01-23 22:19:25 +0900

    test

--- src/test/test.java
+++ src/test/test.java
@@ -1,15 +1,24 @@
 package test;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.function.DoubleUnaryOperator;
+import java.util.stream.Stream;
 
 public class test{
 
     public static void main(String[] args) throws IOException {
-        DoubleUnaryOperator oper;
-        oper = Math::abs;
+        ArrayList<Integer> list = new ArrayList<>();
 
-        System.out.println(oper.applyAsDouble(-5));
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+
+        Stream<Integer> stream = list.stream();
+
+        stream.forEach(System.out::println);
 
     }
 
